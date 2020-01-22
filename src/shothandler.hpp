e6fe6d6c4b24880056c54088410a31d64a9ba594
Nic Jenneboer <nicjenneboer@gmail.com>
2020-01-22 22:18:49 +0100

    shooting update

--- src/shothandler.hpp
+++ src/shothandler.hpp
@@ -6,17 +6,22 @@
 
 class bullet : public sf::CircleShape{
 private:
-	const sf::Vector2f & endPos;
-	const int & tileSize;
-	sf::Vector2f direction;
+	tower * myTower;
+	std::unique_ptr<enemyChar> & enemy;
 	sf::Clock clock;
-	float a; float b;
 
 
 public:
-	bullet( const sf::Vector2f & beginPos, const sf::Vector2f & endPos, const int & tileSize );
+
+	
+
+	bullet( tower * myTower, std::unique_ptr<enemyChar> & enemy );
 
 	void updatePos();
+
+	void hitEnemy();
+
+	bool intersectsEnemy();
 
 };
 
